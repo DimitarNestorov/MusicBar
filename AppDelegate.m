55c091124970893cae46313331f080db7a6ee634
Dimitar Nestorov <dimitarnestorov@users.noreply.github.com>
2020-04-10 06:35:49 +0300

    Freezing popover position once open
    Removing triangle

--- AppDelegate.m
+++ AppDelegate.m
@@ -10,12 +10,44 @@
 
 @property (weak) IBOutlet NSPopover *popover;
 
+@property (weak) IBOutlet NSWindow *positioningWindow;
+@property (weak) IBOutlet NSView *positioningView;
+
 @end
 
 @implementation AppDelegate
 
+- (void)showPopover:(NSStatusBarButton *)sender {
+    if (self.popover.isShown) return;
+
+    NSRect rect = [sender.window convertRectToScreen:sender.frame];
+    CGFloat xOffset = CGRectGetMidX(self.positioningWindow.contentView.frame) - CGRectGetMidX(sender.frame);
+    CGFloat x = rect.origin.x - xOffset;
+    CGFloat y = rect.origin.y;
+    [self.positioningWindow setFrameOrigin:NSMakePoint(x, y)];
+    [self.positioningWindow makeKeyAndOrderFront:self];
+    [self.popover showRelativeToRect:self.positioningView.bounds ofView:self.positioningView preferredEdge:NSMinYEdge];
+    self.positioningView.bounds = CGRectOffset(self.positioningView.bounds, 0, 22);
+    
+    [NSApp activateIgnoringOtherApps:YES];
+}
+
+- (void)popoverDidClose:(NSNotification *)notification {
+    self.positioningView.bounds = CGRectOffset(self.positioningView.bounds, 0, -22);
+}
+
+#pragma mark - Application delegate
+
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
-    [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(infoDidChange:) name:GlobalStateNotification.infoDidChange object:nil];
+    [NSNotificationCenter.defaultCenter addObserver:self
+                                           selector:@selector(infoDidChange:)
+                                               name:GlobalStateNotification.infoDidChange
+                                             object:nil];
+
+    [NSNotificationCenter.defaultCenter addObserver:self
+                                           selector:@selector(popoverDidClose:)
+                                               name:NSPopoverDidCloseNotification
+                                             object:nil];
     
     self.statusItem = [NSStatusBar.systemStatusBar statusItemWithLength:NSVariableStatusItemLength];
     self.statusItem.target = self;
@@ -23,12 +55,11 @@
     self.statusItem.highlightMode = YES;
     
     self.statusItem.button.image = [NSImage imageNamed:@"Note"];
-}
-
-- (void)showPopover:(NSStatusBarButton *)sender {
-    [self.popover showRelativeToRect:sender.frame ofView:sender preferredEdge:NSMaxYEdge];
     
-    [NSApp activateIgnoringOtherApps:YES];
+    self.positioningWindow.opaque = YES;
+    self.positioningWindow.backgroundColor = NSColor.clearColor;
+    self.positioningWindow.level = kCGMaximumWindowLevel | kCGFloatingWindowLevel;
+    self.positioningWindow.ignoresMouseEvents = YES;
 }
 
 - (void)applicationWillResignActive:(NSNotification *)notification {
